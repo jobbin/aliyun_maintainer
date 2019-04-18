006a5d26fdd8882f17fe616fcef1dfde6b9be0b2
jobbin <qiubinbin0615@gmail.com>
2019-04-18 20:12:46 +0900

    RDSイベントの追加、サンプルjson

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aliyun_maintainer
 For keeping Alibaba Cloud cleaning.
+
+Event type of ActionTrail log is saved to OSS Bucket: ObjectCreated:PutObject
--- index.py
+++ index.py
@@ -7,12 +7,11 @@ from aliyunsdkcore.client import AcsClient
 from aliyunsdkcore.acs_exception.exceptions import ClientException
 from aliyunsdkcore.acs_exception.exceptions import ServerException
 from aliyunsdkecs.request.v20140526 import AddTagsRequest
-from aliyunsdkrds.request.v20140815.AddTagsToResourceRequest import AddTagsToResourceRequest
 
 
 def handler(event, context):
   logger = logging.getLogger()
-  logger.info(event)
+  logger.info('initializing')
 
   ACCESS_KEY_ID = os.environ['ACCESS_KEY_ID']
   ACCESS_KEY_SECRET = os.environ['ACCESS_KEY_SECRET']
@@ -24,7 +23,7 @@ def handler(event, context):
   ObjectName = Event["events"][0]["oss"]["object"]["key"]
   OssEndPoint = "oss-" + OssRegion +".aliyuncs.com"
 
-  # ECS, RDS関連初期設定
+  # ECS関連初期設定
   InstanceIdSet = []
   UserName = ""
   Region = ""
@@ -51,8 +50,8 @@ def handler(event, context):
     logger.info("acsRegion : " + actionTrailLog["acsRegion"])
     logger.info("*"*20)
 
-    TARGET_EVENTS = ["RunInstances", "CreateInstance", "CreateDBInstance"]
-    if actionTrailLog["eventName"] in TARGET_EVENTS :
+    ECS = ["RunInstances", "CreateInstance"]
+    if actionTrailLog["eventName"] in ECS :
 
       if actionTrailLog["eventName"] == "RunInstances" :
         InstanceIdSet = actionTrailLog["responseElements"]["InstanceIdSets"]["InstanceIdSet"]
@@ -60,46 +59,27 @@ def handler(event, context):
       if actionTrailLog["eventName"] == "CreateInstance" :
         InstanceIdSet.append(actionTrailLog["responseElements"]["InstanceId"])
 
-      if actionTrailLog["eventName"] == "CreateDBInstance":
-        
-        if actionTrailLog["requestParameters"]["Quantity"] == 1:
-          # Quantity = 1
-          InstanceIdSet.append(actionTrailLog["responseElements"]["DBInstanceId"])
-        else: 
-          # Quantity > 1
-          # TODO
-          # 複数の場合の処理、type(InstanceIdSet) == str !!!!!!!!!
-          InstanceIdSet = actionTrailLog["responseElements"]["DBInstanceId"]
-
-
       #logger.info(InstanceIdSet)
       UserName = actionTrailLog["userIdentity"]["userName"]
-      AcsRegion = actionTrailLog["acsRegion"]
+      EcsRegion = actionTrailLog["acsRegion"]
 
     else:
-      logger.info("Isn't target event !")
+      logger.info("Isn't RunInstances event !")
       return 0
 
-  #TARGET_EVENTS instanceにOwnerタグを追加
-  client = AcsClient(ACCESS_KEY_ID, ACCESS_KEY_SECRET, AcsRegion)
+  #ECS instanceにOwnerタグを追加
+  client = AcsClient(ACCESS_KEY_ID, ACCESS_KEY_SECRET, EcsRegion)
 
 
   for instance in InstanceIdSet :
-    
-    if actionTrailLog["eventName"] == "CreateDBInstance":
-      request = AddTagsToResourceRequest()
-      request.set_accept_format('json')
-      logger.info(instance)
-      # request.set_Tags(str)
-      request.set_Tags({TagName: UserName})
-      request.set_DBInstanceId(instance)
-    else:  
-      Tags = [{"Key": TagName,"Value": UserName}]
-      request = AddTagsRequest.AddTagsRequest()
-      request.set_ResourceType("instance")
-      request.set_Tags(Tags)
-      request.set_ResourceId(instance)
+
+    request = AddTagsRequest.AddTagsRequest()
+    request.set_ResourceType("instance")
+    request.set_ResourceId(instance)
+
+    Tags = [{"Key": TagName,"Value": UserName}]
+    request.set_Tags(Tags)
 
     client.do_action_with_exception(request)
-    print(str(response, encoding='utf-8'))
+
   return 0
